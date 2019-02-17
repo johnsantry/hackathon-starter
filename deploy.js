ece1d620893ba34d17556183b006bec9896d3648
john <johnsantry82@gmail.com>
2019-02-17 13:54:37 -0700

    updated ip in deploy.js

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '3.83.117.167',
+      host: '3.87.71.199',
       username: 'ubuntu',
       privateKey: 'hs2-key.pem'
     })
